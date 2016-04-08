3baf5baa413ce8eb60616640cf84a94aa7b670c2
erminiofabris <erminio.fabris@roncadin.it>
2016-04-08 08:46:33 +0200

    Primo commit

---
+++ GestioneRevisioniOrdiniAcquisto/Form1.b1f.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SAPbouiCOM.Framework;
+
+namespace SBOAddonProject1
+{
+    [FormAttribute("SBOAddonProject1.Form1", "Form1.b1f")]
+    class Form1 : UserFormBase
+    {
+        public Form1()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+    }
+}
